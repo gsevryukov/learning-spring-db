0e87d2fafb7f3b80febbcd300166c15a6f8e39e0
grigory.sevryukov <grigory.sevryukov@x5.ru>
2023-01-23 23:42:59 +0300

    init commit

---
+++ src/main/java/ru/sevryukov/learningspringdb/LearningSpringDbApplication.java
@@ -0,0 +1,13 @@
+package ru.sevryukov.learningspringdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sevryukov/learningspringdb/LearningSpringDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sevryukov.learningspringdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
